36c14bce0d99c6c05db317e750866602f66b3f79
Satyen Rai <satyenr@users.noreply.github.com>
2019-05-22 19:37:25 +0530

    spacemacs: initial check in

---
+++ spacemacs/init.el
@@ -0,0 +1,144 @@
+;; -*- mode: emacs-lisp -*-
+
+(defvar dotspacemacs/layers/core
+  "Core layers"
+  '(auto-completion
+    better-defaults
+    deft
+    emoji
+    git
+    helm
+    (org :variables org-enable-github-support t)
+    osx
+    shell
+    spell-checking
+    syntax-checking
+    version-control))
+
+(defvar dotspacemacs/layers/langs
+  "Language layers"
+  '(emacs-lisp
+    haskell
+    html
+    java
+    javascript
+    markdown
+    python
+    ruby
+    yaml
+    shell-scripts))
+
+(defvar dotspacemacs/layers/local
+  "Local layers"
+  '((config :location local)))
+
+(defun dotspacemacs/layers ()
+  "Configuration layers"
+  (setq-default
+   dotspacemacs-distribution 'spacemacs
+   dotspacemacs-enable-lazy-installation 'unused
+   dotspacemacs-ask-for-lazy-installation t
+   dotspacemacs-configuration-layer-path '()
+   dotspacemacs-configuration-layers (append dotspacemacs/layers/core
+                                             dotspacemacs/layers/langs
+                                             dotspacemacs/layers/local)
+   dotspacemacs-additional-packages '()
+   dotspacemacs-frozen-packages '()
+   dotspacemacs-excluded-packages '()
+   dotspacemacs-install-packages 'used-only))
+
+(defun dotspacemacs/init ()
+  "Initialization function"
+  (setq-default
+   dotspacemacs-elpa-https t
+   dotspacemacs-elpa-timeout 5
+   dotspacemacs-check-for-update nil
+   dotspacemacs-elpa-subdirectory nil
+   dotspacemacs-editing-style 'vim
+   dotspacemacs-verbose-loading nil
+   dotspacemacs-startup-banner 'random
+   dotspacemacs-startup-lists '((todos . 5)
+                                (agenda . 5)
+                                (recents . 5)
+                                (projects . 7))
+   dotspacemacs-startup-buffer-responsive t
+   dotspacemacs-scratch-mode 'text-mode
+   dotspacemacs-themes '(spacemacs-dark
+                         spacemacs-light
+                         zenburn
+                         doom-one
+                         doom-one-light
+                         solarized-dark
+                         solarized-light)
+   dotspacemacs-colorize-cursor-according-to-state t
+   dotspacemacs-default-font '("Source Code Pro"
+                               :size 13
+                               :weight normal
+                               :width normal
+                               :powerline-scale 1.1)
+   dotspacemacs-leader-key "SPC"
+   dotspacemacs-emacs-command-key "SPC"
+   dotspacemacs-ex-command-key ":"
+   dotspacemacs-emacs-leader-key "M-m"
+   dotspacemacs-major-mode-leader-key ","
+   dotspacemacs-major-mode-emacs-leader-key "C-M-m"
+   dotspacemacs-distinguish-gui-tab nil
+   dotspacemacs-remap-Y-to-y$ nil
+   dotspacemacs-retain-visual-state-on-shift t
+   dotspacemacs-visual-line-move-text t
+   dotspacemacs-ex-substitute-global nil
+   dotspacemacs-default-layout-name "Default"
+   dotspacemacs-display-default-layout nil
+   dotspacemacs-auto-resume-layouts nil
+   dotspacemacs-large-file-size 1
+   dotspacemacs-auto-save-file-location 'cache
+   dotspacemacs-max-rollback-slots 10
+   dotspacemacs-helm-resize nil
+   dotspacemacs-helm-no-header nil
+   dotspacemacs-helm-position 'bottom
+   dotspacemacs-helm-use-fuzzy 'always
+   dotspacemacs-enable-paste-transient-state nil
+   dotspacemacs-which-key-delay 0.4
+   dotspacemacs-which-key-position 'bottom
+   dotspacemacs-loading-progress-bar t
+   dotspacemacs-fullscreen-at-startup nil
+   dotspacemacs-fullscreen-use-non-native nil
+   dotspacemacs-maximized-at-startup nil
+   dotspacemacs-active-transparency 90
+   dotspacemacs-inactive-transparency 90
+   dotspacemacs-show-transient-state-title t
+   dotspacemacs-show-transient-state-color-guide t
+   dotspacemacs-mode-line-unicode-symbols t
+   dotspacemacs-smooth-scrolling t
+   dotspacemacs-line-numbers '(:relative nil
+                               :size-limit-kb 1024
+                               :disabled-for-modes doc-view-mode
+                                                   pdf-view-mode
+                                                   text-mode)
+   dotspacemacs-folding-method 'evil
+   dotspacemacs-smartparens-strict-mode nil
+   dotspacemacs-smart-closing-parenthesis nil
+   dotspacemacs-highlight-delimiters 'all
+   dotspacemacs-persistent-server nil
+   dotspacemacs-search-tools '("ag" "pt" "ack" "grep")
+   dotspacemacs-default-package-repository nil
+   dotspacemacs-whitespace-cleanup trailing
+   ))
+
+(defun dotspacemacs/user-init ()
+  "User initialization function"
+  (setq custom-file (file-truename (concat dotspacemacs-directory "custom.el"))))
+
+(defun dotspacemacs/user-config ()
+  "User configuration"
+  (setq deft-directory "~/Documents/Deft"
+        deft-extensions '("txt" "md" "org")
+        deft-recursive t)
+  (custom-set-faces
+   '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
+   '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
+   '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+   '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+   '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+   )
+  )
