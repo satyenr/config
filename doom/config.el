efa76b35348672b6379d0f9c056f2ba4643fb14f
Satyen Rai <satyenr@users.noreply.github.com>
2019-05-25 17:54:25 +0530

    doom: check in initial doom-emacs config

---
+++ doom/config.el
@@ -0,0 +1,15 @@
+;;; -*- lexical-binding: t; -*-
+
+;; deft
+(setq deft-directory "~/Documents/Notes")
+(setq deft-extensions '("txt" "md" "org"))
+(setq deft-use-filter-string-for-filename t)
+(setq deft-recursive t)
+
+;; org-mode
+(setq org-directory "~/Documents/Notes")
+
+;; org-journal
+(setq org-journal-dir "~/Documents/Notes/journal")
+(setq org-journal-file-type 'yearly)
+(setq org-journal-file-format "%Y")
